b64cef19bfc250b5abccc19203b0d0d2f5147cf0
Rajib Chowdhury <rajib.c@crri.co.in>
2010-06-28 11:59:20 +0530

    indention updated for view_helpers.rb and install.rb

--- install.rb
+++ install.rb
@@ -1,33 +1,33 @@
 # Workaround a problem with script/plugin and http-based repos.
 # See http://dev.rubyonrails.org/ticket/8189
 Dir.chdir(Dir.getwd.sub(/vendor.*/, '')) do
-	##
-	## Copy over files (javascript/css/images) from the plugin directory to public/
-	##
+  ##
+  ## Copy over files (javascript/css/images) from the plugin directory to public/
+  ##
 
-	def copy_files(source_path, destination_path, directory)
-		source, destination = File.join(directory, source_path), File.join(Rails.root, destination_path)
-		FileUtils.mkdir_p(destination) unless File.exist?(destination)
-		FileUtils.cp_r(Dir.glob(source+'/*.*'), destination)
-	end
+  def copy_files(source_path, destination_path, directory)
+    source, destination = File.join(directory, source_path), File.join(Rails.root, destination_path)
+    FileUtils.mkdir_p(destination) unless File.exist?(destination)
+    FileUtils.cp_r(Dir.glob(source+'/*.*'), destination)
+  end
 
-	directory = File.dirname(__FILE__)
+  directory = File.dirname(__FILE__)
 
-	copy_files("/public", "/public", directory)
+  copy_files("/public", "/public", directory)
 
-	jqpageflow_resources = Dir[File.join(directory, 'jqpageflow', '*')].collect { |d| File.basename d }
-	[ :stylesheets, :javascripts, :images].each do |file_type|
-		path = "/public/#{file_type}/scroll_paginate"
-		copy_files(path, path, directory)
-		
-		File.open(File.join(Rails.root, path, 'DO_NOT_EDIT'), 'w') do |f|
-		  f.puts "Any changes made to files in sub-folders will be lost."
-		end
+  jqpageflow_resources = Dir[File.join(directory, 'jqpageflow', '*')].collect { |d| File.basename d }
+  [ :stylesheets, :javascripts, :images].each do |file_type|
+    path = "/public/#{file_type}/scroll_paginate"
+    copy_files(path, path, directory)
 
-		jqpageflow_resources.each do |jqpageflow_resource|
-		  source = "/jqpageflow/#{jqpageflow_resource}/#{file_type}/"
-		  destination = "/public/#{file_type}/scroll_paginate/#{jqpageflow_resource}"
-		  copy_files(source, destination, directory)
-		end
-	end
+    File.open(File.join(Rails.root, path, 'DO_NOT_EDIT'), 'w') do |f|
+      f.puts "Any changes made to files in sub-folders will be lost."
+    end
+
+    jqpageflow_resources.each do |jqpageflow_resource|
+      source = "/jqpageflow/#{jqpageflow_resource}/#{file_type}/"
+      destination = "/public/#{file_type}/scroll_paginate/#{jqpageflow_resource}"
+      copy_files(source, destination, directory)
+    end
+  end
 end
--- lib/scroll_paginate/helpers/view_helpers.rb
+++ lib/scroll_paginate/helpers/view_helpers.rb
@@ -1,61 +1,61 @@
 module ScrollPaginate
-	module Helpers
-		module ViewHelpers
-			# DESCRIPTION: will generete the javascript block for enebaling 
-			#	scroll pagination.
-			# EXAMPLE: <%= scroll_paginate(:url => 'http://example.com',
-			#							:total_results => 100,
-			#							:container => "#mycontainer",
-			#							:current_page => 0, 
-			#							:per_page => 20, 
-			#							:pager_var => 'p',
-			#							:loader_img_path => 'images/loader.gif', 
-			#							:debug => 1) %>
-			# :url ( required field :EXAMPLE => 'http://example.com' )
-			# :total_results ( required field :EXAMPLE => User.count )
-			# :container ( required field :EXAMPLE => Container Div for scroll pagination )
-			#	:current_page ( optional :DESCRIPTION => Gets appended to url as a GET value to help your backend 
-			#	script keep track of which page of results the user is on. Defaults to 0 (first page) ), 
-			#	:per_page ( optional :DESCRIPTION => Used for calculation and display purposes, tell the plugin how 
-			#	many results are being displayed per page. Defaults to 50.), 
-			#	:pager_var ( optional :DESCRIPTION => Related to currentPage, gets appended to url as a GET var to help 
-			# your backend script keep track of which page of results the user is on (e.g. index.php?page=2). 
-			# Defaults to "p" ),
-			#	:loader_img_path ( optional :DESCRIPTION => Tell the plugin where to find the loader img relative to 
-			#	the page calling the plugin. Defaults to "images/scroll_paginate/default/loader.gif" ), 
-			#	:debug ( optional :DESCRIPTION => When set to 1, the plugin will print debugging information to the 
-			#	console (firebug). Defaults to 0  ) %>
+  module Helpers
+    module ViewHelpers
+      # DESCRIPTION: will generete the javascript block for enebaling 
+      #	scroll pagination.
+      # EXAMPLE: <%= scroll_paginate(:url => 'http://example.com',
+      #							:total_results => 100,
+      #							:container => "#mycontainer",
+      #							:current_page => 0, 
+      #							:per_page => 20, 
+      #							:pager_var => 'p',
+      #							:loader_img_path => 'images/loader.gif', 
+      #							:debug => 1) %>
+      # :url ( required field :EXAMPLE => 'http://example.com' )
+      # :total_results ( required field :EXAMPLE => User.count )
+      # :container ( required field :EXAMPLE => Container Div for scroll pagination )
+      #	:current_page ( optional :DESCRIPTION => Gets appended to url as a GET value to help your backend 
+      #	script keep track of which page of results the user is on. Defaults to 0 (first page) ), 
+      #	:per_page ( optional :DESCRIPTION => Used for calculation and display purposes, tell the plugin how 
+      #	many results are being displayed per page. Defaults to 50.), 
+      #	:pager_var ( optional :DESCRIPTION => Related to currentPage, gets appended to url as a GET var to help 
+      # your backend script keep track of which page of results the user is on (e.g. index.php?page=2). 
+      # Defaults to "p" ),
+      #	:loader_img_path ( optional :DESCRIPTION => Tell the plugin where to find the loader img relative to 
+      #	the page calling the plugin. Defaults to "images/scroll_paginate/default/loader.gif" ), 
+      #	:debug ( optional :DESCRIPTION => When set to 1, the plugin will print debugging information to the 
+      #	console (firebug). Defaults to 0  ) %>
 
-			def	scroll_paginate(options)
-				# initialize variables
-				setup_options(options)
-				html = "<script type='text/javascript'>
-									$(document).ready(function(){
-								  	$('body').flexiPagination({
-								  		url: '#{@url}',
-											totalResults: #{@total_results},
-											container: '#{@container}',								 			
-											currentPage: #{@current_page},
-								 			perPage: #{@per_page},
-								 			pagerVar : '#{@pager_var}',
-								 			loaderImgPath: '#{@loader_img_path}',
-								 			debug : #{@debug}
-								  	});
-									});
-								</script>"
-			end
+      def	scroll_paginate(options)
+        # initialize variables
+        setup_options(options)
+        html = "<script type='text/javascript'>
+          $(document).ready(function(){
+            $('body').flexiPagination({
+                url: '#{@url}',
+                totalResults: #{@total_results},
+                container: '#{@container}',								 			
+                currentPage: #{@current_page},
+                perPage: #{@per_page},
+                pagerVar : '#{@pager_var}',
+                loaderImgPath: '#{@loader_img_path}',
+                debug : #{@debug}
+              });
+            });
+            </script>"
+          end
 
-			protected
-			def	setup_options(options)
-				@url = options[:url]
-				@total_results = options[:total_results]
-				@container = options[:container]
-				@current_page = options[:current_page] || 0
-				@per_page = options[:per_page] || 20
-				@pager_var = options[:pager_var] || 'p'
-				@loader_img_path = options[:loader_img_path] || 'images/scroll_paginate/default/loader.gif'
-				@debug = options[:debug] || 1
-			end
-		end
-	end
-end
+          protected
+          def	setup_options(options)
+            @url = options[:url]
+            @total_results = options[:total_results]
+            @container = options[:container]
+            @current_page = options[:current_page] || 0
+            @per_page = options[:per_page] || 20
+            @pager_var = options[:pager_var] || 'p'
+            @loader_img_path = options[:loader_img_path] || 'images/scroll_paginate/default/loader.gif'
+            @debug = options[:debug] || 1
+          end
+        end
+      end
+    end
