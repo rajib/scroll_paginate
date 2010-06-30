1b853c0a79f5fcd94f7079d3e961ce154073e3b0
Rajib Chowdhury <chowdhury284@gmail.com>
2010-07-01 01:25:46 +0530

    new helper method added for including js and css to layout.

--- lib/scroll_paginate/helpers/view_helpers.rb
+++ lib/scroll_paginate/helpers/view_helpers.rb
@@ -1,29 +1,36 @@
 module ScrollPaginate
   module Helpers
     module ViewHelpers
-      # DESCRIPTION: will generete the javascript block for enebaling 
+      # DECRIPTION: will add require css and javascript file to layout.
+      # EXAMPLE: <%= include_scroll_paginate %>
+      def include_scroll_paginate
+        html = '<link href="/stylesheets/scroll_paginate/default/jqpageflow.css" media="screen" rel="stylesheet" type="text/css" />
+        <script src="/javascripts/scroll_paginate/default/jquery.jqpageflow.js" type="text/javascript"></script>'
+      end
+
+      # DESCRIPTION: will generete the javascript block for enebaling
       #	scroll pagination.
       # EXAMPLE: <%= scroll_paginate(:url => 'http://example.com',
       #                              :total_results => 100,
       #                              :container => "mycontainer",
-      #                              :current_page => 0, 
-      #                              :per_page => 20, 
+      #                              :current_page => 0,
+      #                              :per_page => 20,
       #                              :pager_var => 'p',
-      #                              :loader_img_path => 'images/loader.gif', 
+      #                              :loader_img_path => 'images/loader.gif',
       #                              :debug => 1) %>
       # :url ( required field :EXAMPLE => 'http://example.com' )
       # :total_results ( required field :EXAMPLE => User.count )
       # :container ( required field :EXAMPLE => Container Div for scroll pagination )
-      #	:current_page ( optional :DESCRIPTION => Gets appended to url as a GET value to help your backend 
-      #	script keep track of which page of results the user is on. Defaults to 0 (first page) ), 
-      #	:per_page ( optional :DESCRIPTION => Used for calculation and display purposes, tell the plugin how 
-      #	many results are being displayed per page. Defaults to 50.), 
-      #	:pager_var ( optional :DESCRIPTION => Related to currentPage, gets appended to url as a GET var to help 
-      # your backend script keep track of which page of results the user is on (e.g. index.php?page=2). 
+      #	:current_page ( optional :DESCRIPTION => Gets appended to url as a GET value to help your backend
+      #	script keep track of which page of results the user is on. Defaults to 0 (first page) ),
+      #	:per_page ( optional :DESCRIPTION => Used for calculation and display purposes, tell the plugin how
+      #	many results are being displayed per page. Defaults to 50.),
+      #	:pager_var ( optional :DESCRIPTION => Related to currentPage, gets appended to url as a GET var to help
+      # your backend script keep track of which page of results the user is on (e.g. index.php?page=2).
       # Defaults to "p" ),
-      #	:loader_img_path ( optional :DESCRIPTION => Tell the plugin where to find the loader img relative to 
-      #	the page calling the plugin. Defaults to "images/scroll_paginate/default/loader.gif" ), 
-      #	:debug ( optional :DESCRIPTION => When set to 1, the plugin will print debugging information to the 
+      #	:loader_img_path ( optional :DESCRIPTION => Tell the plugin where to find the loader img relative to
+      #	the page calling the plugin. Defaults to "images/scroll_paginate/default/loader.gif" ),
+      #	:debug ( optional :DESCRIPTION => When set to 1, the plugin will print debugging information to the
       #	console (firebug). Defaults to 0  ) %>
       def scroll_paginate(options)
         # initialize variables
@@ -33,7 +40,7 @@ module ScrollPaginate
             $('body').flexiPagination({
                 url: '#{@url}',
                 totalResults: #{@total_results},
-                container: '#{@container}',								 			
+                container: '#{@container}',
                 currentPage: #{@current_page},
                 perPage: #{@per_page},
                 pagerVar : '#{@pager_var}',
@@ -44,7 +51,7 @@ module ScrollPaginate
             </script>"
       end
 
-      protected
+      private
       def setup_options(options)
         @url = options[:url]
         @total_results = options[:total_results]
@@ -58,3 +65,4 @@ module ScrollPaginate
     end
   end
 end
+
