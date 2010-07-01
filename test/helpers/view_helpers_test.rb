daf80641b2c2afd3acee12b62a3391d9e3992075
Rajib Chowdhury <rajib.c@crri.co.in>
2010-07-01 10:26:09 +0530

    view_helpers_test.rb code indented properly.

--- test/helpers/view_helpers_test.rb
+++ test/helpers/view_helpers_test.rb
@@ -3,11 +3,11 @@ require File.join(File.dirname(__FILE__), '../test_helper.rb')
 class ViewHelpersTest < ActionView::TestCase
   include ScrollPaginate::Helpers::ViewHelpers
 
-	context "have scroll_paginate" do
-	  should  "return true" do
+  context "have scroll_paginate" do
+    should  "return true" do
       assert scroll_paginate(:url => 'http://example.com', :total_results => 100,
-													   :container => "mycontainer")
-		end
+      :container => "mycontainer")
+    end
   end
 
   context "generate scroll_pagination " do
@@ -15,39 +15,39 @@ class ViewHelpersTest < ActionView::TestCase
       setup do
         @result_array = []
         @expected_array = ["<script type='text/javascript'>", "$(document).ready(function(){", "$('body').flexiPagination({",
-                           "url: 'http://example.com',", "totalResults: 100,", "container: '#mycontainer',", "currentPage: 0,",
-                           "perPage: 20,", "pagerVar : 'p',", "loaderImgPath: 'images/scroll_paginate/default/loader.gif',",
-                           "debug : 1", "});", "});", "</script>"]
+        "url: 'http://example.com',", "totalResults: 100,", "container: '#mycontainer',", "currentPage: 0,",
+        "perPage: 20,", "pagerVar : 'p',", "loaderImgPath: 'images/scroll_paginate/default/loader.gif',",
+        "debug : 1", "});", "});", "</script>"]
 
         result_html = scroll_paginate(:url => 'http://example.com', :total_results => 100,
-                                         :container => "mycontainer")
+        :container => "mycontainer")
         result_html.each {|s| @result_array << s.strip}
       end
 
-  	  should "return true when result_array and expected_array are same" do
-  		  assert_equal @result_array, @expected_array
-  	  end
+      should "return true when result_array and expected_array are same" do
+        assert_equal @result_array, @expected_array
+      end
     end
 
     context "with maximum option" do
       setup do
         @result_array = []
         @expected_array = ["<script type='text/javascript'>", "$(document).ready(function(){", "$('body').flexiPagination({",
-                           "url: 'http://abc.com',", "totalResults: 200,", "container: '#mycustomcontainer',", "currentPage: 2,",
-                           "perPage: 10,", "pagerVar : 'page',", "loaderImgPath: 'images/custom/loader.gif',",
-                           "debug : 1", "});", "});", "</script>"]
+        "url: 'http://abc.com',", "totalResults: 200,", "container: '#mycustomcontainer',", "currentPage: 2,",
+        "perPage: 10,", "pagerVar : 'page',", "loaderImgPath: 'images/custom/loader.gif',",
+        "debug : 1", "});", "});", "</script>"]
 
         result_html = scroll_paginate(:url => 'http://abc.com', :total_results => 200,
-                                      :container => "mycustomcontainer", :current_page => 2,
-                                      :per_page => 10, :pager_var => 'page', :loader_img_path => 'images/custom/loader.gif',
-                                      :debug => 1)
+        :container => "mycustomcontainer", :current_page => 2,
+        :per_page => 10, :pager_var => 'page', :loader_img_path => 'images/custom/loader.gif',
+        :debug => 1)
         result_html.each {|s| @result_array << s.strip}
       end
 
-  	  should "return true when result_array and expected_array are same" do
-  		  assert_equal @result_array, @expected_array
-  	  end
+      should "return true when result_array and expected_array are same" do
+        assert_equal @result_array, @expected_array
+      end
     end
-	end
+  end
 end
 
